30617680e600ad90767900fe1baf8cf5be1d64fa
shivam24s <shivamshrivastav8055@gmail.com>
2026-05-21 11:20:48 +0530

    string lab code added

---
+++ C/15_function/01_function.c
@@ -0,0 +1,42 @@
+
+
+#include <stdio.h>
+
+// function declaration
+void sum(int a, int b)
+{
+
+    printf("\nthe sum of two number is %d", a + b);
+}
+
+int main()
+{
+
+    // using repeated method
+
+    // int a = 10;
+
+    // int b = 20;
+
+    // printf("\nthe sum of two number is %d", a + b);
+
+    // int c = 30;
+
+    // int d = 40;
+
+    // printf("\nthe sum of two number is %d", c + d);
+
+    // now using function
+
+
+
+    // function invocation 
+    sum(10, 20);
+    sum(30, 40);
+
+
+
+    
+
+    return 0;
+}
