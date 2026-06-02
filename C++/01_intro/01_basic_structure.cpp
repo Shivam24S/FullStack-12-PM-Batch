3362bbafdb263fa4c77a0c99f4a0b4f1813ecec9
shivam24s <shivamshrivastav8055@gmail.com>
2026-06-02 13:16:49 +0530

    c++ basic structure code added

---
+++ C++/01_intro/01_basic_structure.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // std::cout << "hello from c++";
+
+    cout << "hello from c++";
+
+    return 0;
+}
---
+++ C++/01_intro/02_cin_and_cout.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string name;
+
+    cout << "enter your name ";
+
+    cin >> name;
+
+    cout << "hi there " << name;
+
+    return 0;
+}
