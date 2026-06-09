719520257a88eea1073f802c4a57397b2d42120f
shivam24s <shivamshrivastav8055@gmail.com>
2026-06-09 12:53:13 +0530

    reference variable example code added

---
+++ C++/04_data_encapsulation/4.4/03_reference_variable_in_function.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+void sum(int &x)
+{
+    x = 100;
+}
+
+int main()
+{
+
+    int num = 10;
+
+    cout << "num value " << num << endl;
+
+    sum(num);
+
+    cout << "num value after " << num << endl;
+
+    return 0;
+}
