8b84933c1efd963b3272dff8daf3d08f76532fe7
shivam24s <shivamshrivastav8055@gmail.com>
2026-07-07 13:11:18 +0530

    variable concept code added

--- Javascript/02_JS_intro_and_concepts/01_var.js
+++ Javascript/02_JS_intro_and_concepts/01_var.js
@@ -11,3 +11,49 @@ function currentActivity() {
 // console.log("activity :-", activity);
 
 currentActivity();
+
+
+
+// var declaration
+
+
+var num;
+
+console.log("num", num)
+
+
+// var initialization
+
+num = 24;
+
+console.log("num", num)
+
+
+// reinitialization
+
+num = 20;
+
+console.log("num", num)
+
+
+
+var game = "gta-5";
+
+
+console.log(game, "game")
+
+var game = "eurotruck"
+
+console.log("new game", game)
+
+
+
+function play() {
+
+  var game = "snowrunner"
+
+  console.log("i am playing ", game)
+
+}
+
+play()
---
+++ Javascript/02_JS_intro_and_concepts/03_const.js
@@ -0,0 +1,32 @@
+function greeting() {
+    const message = "good morning";
+
+
+}
+
+
+
+greeting()
+
+
+// console.log("message", message);
+
+
+
+const game ="god of war";
+
+console.log("game", game)
+
+
+// game = "ms flight simulator"
+
+// console.log("game", game)
+
+
+
+const game = "forza"
+
+console.log("game", game)
+
+
+
