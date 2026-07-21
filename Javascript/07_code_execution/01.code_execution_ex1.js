3238b4fb48bfa47486ec1444eda5ddb7233d1929
shivam24s <shivamshrivastav8055@gmail.com>
2026-07-21 13:00:20 +0530

    code execution and hoisting concept code added

--- Javascript/07_code_execution/01.code_execution_ex1.js
+++ Javascript/07_code_execution/01.code_execution_ex1.js
@@ -1,13 +1,29 @@
-var n = 2;
+// var n = 2;
 
-function square(num) {
-    var result = num * num;
+// function square(num) {
+//     var result = num * num;
 
-    return result;
-}
-
-
-var square2 = square(n);
+//     return result;
+// }
 
 
-var square4 = square(4)
+// var square2 = square(n);
+
+
+// var square4 = square(4)
+
+
+// let n = 2;
+
+// function square(num) {
+//     let result = num * num;
+
+//     return result;
+// }
+
+
+// let square2 = square(n);
+
+
+// const  square4 = square(4)
+
---
+++ Javascript/07_code_execution/02_hoisting.js
@@ -0,0 +1,53 @@
+
+
+// function declaration
+
+// game();
+
+
+// function game() {
+//     console.log("i am playing ms flight simulator");
+// }
+
+
+
+
+// function expression
+// activity()
+// const activity = () => {
+
+//     console.log("currently i am running")
+// }
+
+
+
+// variable hoisting
+
+
+// hoistedVar = "dexter";
+
+// console.log(hoistedVar)
+
+// var hoistedVar
+
+
+// not hoisted
+
+
+// var hoistedVar
+
+// console.log(hoistedVar)
+
+// hoistedVar = "dexter";
+
+
+
+// using let and const 
+
+
+// hoistedLetVar = "g-wagon"
+
+
+// console.log(hoistedLetVar)
+
+// let hoistedLetVar
