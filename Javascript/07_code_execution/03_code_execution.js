d6f5e3516132facc5c7d08d6d6fc477b4fe1105d
shivam24s <shivamshrivastav8055@gmail.com>
2026-07-27 13:06:42 +0530

    undefined and not defined

---
+++ Javascript/07_code_execution/03_code_execution.js
@@ -0,0 +1,24 @@
+var x = 1000;
+
+a();
+b();
+
+console.log(x);
+
+function a() {
+  var x = 10;
+
+  console.log(x);
+}
+
+function b() {
+  var x = 100;
+
+  console.log(x);
+}
+
+
+
+// 10
+// 100
+// 1000
---
+++ Javascript/07_code_execution/06_lexical.js
@@ -0,0 +1,16 @@
+// const x = 1000;
+
+function parent() {
+
+    // let x = 100;
+
+    function child() {
+        // let x = 200;
+
+        console.log("x value", x);
+    }
+
+    child()
+}
+
+parent()
