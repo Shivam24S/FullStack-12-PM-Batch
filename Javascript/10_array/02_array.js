756b1da0ad10100d4a43edb7a9d0d38653dcb35f
shivam24s <shivamshrivastav8055@gmail.com>
2026-08-06 13:01:52 +0530

    array methods code added

---
+++ Javascript/10_array/02_array.js
@@ -0,0 +1,16 @@
+
+
+const fruits = ["apple","banana","cherry","dragon-fruits","orange","mango"]
+
+
+console.log("fruits",fruits)
+
+console.log("fruits",fruits[0])
+
+console.log("fruits",fruits[fruits.length-1])
+
+
+fruits[0] ="pineapple"
+
+console.log("fruits",fruits[0])
+
---
+++ Javascript/10_array/03_array.js
@@ -0,0 +1,35 @@
+
+
+const fruits = ["apple","banana","cherry","dragon-fruits","orange","mango"]
+
+
+fruits.push("strawberry")
+
+console.log("fruits",fruits)
+
+
+fruits.unshift("kiwi")
+
+console.log("fruits",fruits)
+
+fruits.pop()
+
+console.log("fruits",fruits)
+
+fruits.shift()
+
+console.log("fruits",fruits)
+
+
+
+console.log("fruits array",fruits)
+
+
+fruits.splice(3,0,"popeye")
+
+console.log("fruits array",fruits)
+
+fruits.splice(3,2,"guava")
+
+console.log("fruits array",fruits)
+
