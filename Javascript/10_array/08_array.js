c2b68955403ea6e33e5217d4f3c13469ff6e529c
shivam24s <shivamshrivastav8055@gmail.com>
2026-08-11 12:50:31 +0530

    array method code added

--- Javascript/10_array/08_array.js
+++ Javascript/10_array/08_array.js
@@ -1,8 +1,47 @@
+const fruits = ["apple", "banana", "cherry", "dragon fruits", "pineapple"];
+
+console.log("index of", fruits.indexOf("mango"));
+
+
+
+console.log(fruits.includes("mango"))
+
+
+
+let alphabet = ["a", "d", "b", "e", "c"];
+
+let numbers = [1,9,7,5,2,1,0,8]
+
+
+console.log(alphabet.sort())
+
+console.log(numbers.sort())
+
+
+
+const descendingNumber = numbers.sort((a,b)=>{
+    return b-a
+})
+
+
+a>b 
 
 
 
 
-const fruits = ["apple","banana","cherry","dragon fruits","pineapple"]
+
+console.log("descending numbers",descendingNumber)
 
 
-console.log("index of",fruits.indexOf("mango"))
+let name = "dexter";
+
+let nameArray = name.split("");
+
+console.log("name array",nameArray.reverse())
+
+let reverseName = nameArray.join("");
+
+console.log("reverseName",reverseName)
+
+
+
---
+++ Javascript/10_array/09_array.js
@@ -0,0 +1,23 @@
+
+
+
+// find 
+
+
+let numbers = [1,4,5,6,87,98,9,4,47,87,5,41,4,74]
+
+// numbers.sort();
+
+
+let findResult = numbers.find((num)=>num>24)
+
+console.log("find result",findResult)
+
+
+
+// findIndex
+
+
+let findIndexResult = numbers.findIndex((num)=>num>24)
+
+console.log("find index result",findIndexResult)
